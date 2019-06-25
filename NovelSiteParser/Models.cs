9f44fc6ff80bfe031651e8c4af9172048771e291
Tennyleaz <tennyleaz@gmail.com>
2019-06-25 10:34:13 +0800

    Add model equatable test.

--- NovelSiteParser/Models.cs
+++ NovelSiteParser/Models.cs
@@ -19,31 +19,79 @@ namespace NovelSiteParser
     /// <summary>
     /// 目錄的頁面
     /// </summary>
-    public class BookLink
+    public class BookLink : IEquatable<BookLink>
     {
         public string Title;
         public string Author;
         public string IndexPage;
         public DateTime LastUpdateTime;
         public List<IssueLink> IssueLinks = new List<IssueLink>();
+
+        public bool Equals(BookLink book)
+        {
+            if (book == null)
+                return false;
+            if (Object.ReferenceEquals(this, book))
+                return true;
+            if (IndexPage != book.IndexPage)
+                return false;
+            if (IssueLinks == null && book.IssueLinks == null)
+                return (IndexPage == book.IndexPage) && (Title == book.Title);
+            if (IssueLinks?.Count != book.IssueLinks?.Count)
+                return false;
+            for (int i = 0; i < IssueLinks.Count; i++)
+            {
+                if (!IssueLinks[i].Equals(book.IssueLinks[i]))
+                    return false;
+            }
+            return true;
+        }
     }
 
     /// <summary>
     /// 每一冊的資料
     /// </summary>
-    public class IssueLink
+    public class IssueLink : IEquatable<IssueLink>
     {
         public string Title;
         public List<ChapterLink> ChapterLinks = new List<ChapterLink>();
+
+        public bool Equals(IssueLink issue)
+        {
+            if (issue == null)
+                return false;
+            if (Object.ReferenceEquals(this, issue))
+                return true;
+            if (ChapterLinks == null && issue.ChapterLinks == null)
+                return Title == issue.Title;
+            if (ChapterLinks?.Count != issue.ChapterLinks?.Count)
+                return false;
+            for (int i=0; i<ChapterLinks.Count; i++)
+            {
+                if (!ChapterLinks[i].Equals(issue.ChapterLinks[i]))
+                    return false;
+            }
+            return true;
+        }
     }
 
     /// <summary>
     /// 每一章節的資料
     /// </summary>
-    public class ChapterLink
+    public class ChapterLink : IEquatable<ChapterLink>
     {
         public string Title;
         public string Url;
+        public bool Downloaded;
+
+        public bool Equals(ChapterLink c)
+        {
+            if (c == null)
+                return false;
+            if (Object.ReferenceEquals(this, c))
+                return true;
+            return Url == c.Url;
+        }
     }
 
     public class Book
